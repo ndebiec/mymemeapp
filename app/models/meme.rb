d1b32b6d2d0f07306b4804935568e39a4fe69ab5
ndebiec <ndebiec@wp.pl>
2019-01-07 18:04:11 +0100

    Proper setup for meme upload in development

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -1,4 +1,12 @@
 class Meme < ApplicationRecord
   belongs_to :user
   mount_uploader :picture, PictureUploader
+  validate :picture_size
+  
+  private
+  def picture_size
+    if picture.size > 2.megabytes
+      errors.add(:picture, "File size cannot be greater than 2MB")
+    end
+  end
 end
