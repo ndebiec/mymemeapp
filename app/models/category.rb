f883803b640a9ed88a690c9fbcabbab1589d3676
ndebiec <ndebiec@wp.pl>
2019-01-09 13:55:14 +0100

    Fixed db after creating and dropping categories

--- app/models/category.rb
+++
@@ -1,4 +0,0 @@
-class Category < ApplicationRecord
-  validates :name, presence: true, length: { minimum: 3, maximum: 25}
-  validates_uniquness_of :name
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_08_132834) do
+ActiveRecord::Schema.define(version: 2019_01_08_103706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_categories_on_user_id"
-  end
 
   create_table "memes", force: :cascade do |t|
     t.string "caption"
@@ -60,7 +54,6 @@ ActiveRecord::Schema.define(version: 2019_01_08_132834) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "categories", "users"
   add_foreign_key "memes", "users"
   add_foreign_key "memes_tags", "memes"
   add_foreign_key "memes_tags", "tags"
