4d4dcfd2796b5ff49dafeb1d29b9d6b9f89b82db
ndebiec <ndebiec@wp.pl>
2019-02-27 16:46:24 +0100

    Added provider column to User table

---
+++ db/migrate/20190227154329_add_provider_to_user.rb
@@ -0,0 +1,6 @@
+class AddProviderToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :provider, :string
+    add_column :users, :image, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_27_142120) do
+ActiveRecord::Schema.define(version: 2019_02_27_154329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 2019_02_27_142120) do
     t.string "uid"
     t.string "name"
     t.string "avatar"
+    t.string "provider"
+    t.text "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
