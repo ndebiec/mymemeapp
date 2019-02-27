100175073598c650c96d3a792ad3c0842a11b969
ndebiec <ndebiec@wp.pl>
2019-02-27 17:02:28 +0100

    Removed avatar column, image is enough

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_27_154329) do
+ActiveRecord::Schema.define(version: 2019_02_27_160100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +52,6 @@ ActiveRecord::Schema.define(version: 2019_02_27_154329) do
     t.string "token"
     t.string "uid"
     t.string "name"
-    t.string "avatar"
     t.string "provider"
     t.text "image"
     t.index ["email"], name: "index_users_on_email", unique: true
